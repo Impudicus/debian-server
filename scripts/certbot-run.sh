3b117c79a50ab20bcd7c53ef33402c22abeef112
Impudicus <141483926+Impudicus@users.noreply.github.com>
2023-12-24 23:15:37 +0100

    new commit

--- scripts/certbot-run.sh
+++ scripts/certbot-run.sh
@@ -16,18 +16,53 @@ container_status()
     fi
 }
 
+output_error()
+{
+    /usr/local/bin/notification-push.sh "certbot" "error" "$1"
+    exit 1
+}
+output_info()
+{
+    /usr/local/bin/notification-push.sh "certbot" "info" "$1"
+    exit 1
+}
+output_okay()
+{
+    /usr/local/bin/notification-push.sh "certbot" "okay" "$1"
+    exit 0
+}
+
 # ========================= ========================= =========================
 # MAIN
 
 job_runtime=$SECONDS
 
-# get container status: certbot
 container_status "certbot"
 if [ $? -ne 0 ]; then
-    /usr/local/bin/notification-push.sh "certbot" "info" "job failed (certbot already running)!"
-    exit 1
+    output_info "job failed (certbot already running)!"
 fi
 
+container_start "certbot"
+if [ $? -ne 0 ]; then
+    output_error "job failed (unable to start container)!"
+fi
+
+# check container runstate every 10sec for 5min: certbot
+starttime=$SECONDS
+endtime=$(( SECONDS + 300 ))
+
+while [ $SECONDS -lt $endtime ]; do
+
+    container_status "certbot"
+    if [ $? -eq 0 ]; then
+
+        job_duration=$(($SECONDS - runtime))
+        output_okay "job finished successfully (runtime: $job_duration sec)!"
+    else
+        sleep 10
+    fi
+done
+
 job_duration=$(($SECONDS - runtime))
-/usr/local/bin/notification-push.sh "certbot" "okay" "job finished successfully (runtime: $job_duration sec)!"
+output_error "job failed (timeout: $job_duration sec)!"
 exit 0
