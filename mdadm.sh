1d63034066949d26cf816c02593eef959f89b032
Impudicus <141483926+Impudicus@users.noreply.github.com>
2023-12-29 12:12:16 +0100

    new commit

--- mdadm.sh
+++ mdadm.sh
@@ -100,7 +100,7 @@ echo "--------------------------------------------------"
 echo "INFO: Searching for RAID-Configurations ..."
 used_array=$(cat /etc/mdadm/mdadm.conf | grep ARRAY | awk '{print $2}')
 
-if [[ $used_array == "/dev/md/${HOSTNAME}:0" ]]; then
+if [ $used_array == "/dev/md/${HOSTNAME}:0" ]; then
     echo "INFO: RAID-Configuration found!"
     echo "INFO: $used_array."
     echo "--------------------------------------------------"
