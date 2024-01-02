ae4f2311f0b660e8c9e128dcc533aa8cc07003cb
Impudicus <141483926+Impudicus@users.noreply.github.com>
2024-01-02 21:27:30 +0100

    new commit

--- mdadm.sh
+++ mdadm.sh
@@ -45,7 +45,7 @@ if [ -n "$used_volume" ] && [ -n "$used_array" ]; then
 
         # add raid-volume to fstab
         volume_uuid=$(echo $used_volume | cut -d '"' -f2)
-        echo "UUID=$volume_uuid /mnt/pool1 ext4 defaults,nofail 0 0" >> "/etc/fstab" || exit 1
+        echo "UUID=$volume_uuid /mnt/pool1 ext4 defaults,nofail,errors=remount-ro 0 0" >> "/etc/fstab" || exit 1
 
         # restart daemon
         systemctl daemon-reload || exit 1
