08bbb7d73c6fb701b7eea4165150bd9fa1defa56
Impudicus <141483926+Impudicus@users.noreply.github.com>
2024-01-04 22:20:54 +0100

    new commit

--- scripts/nextcloud-cron.sh
+++ scripts/nextcloud-cron.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Copyright 2023 by Philipp Hildebrandt
 
+container_exec()
+{
+    /usr/bin/docker exec -u $3 -t $1 $2
+    return $?
+}
 container_status()
 {
     result=$(/usr/bin/docker inspect -f "{{.State.Status}}" "$1")
@@ -24,7 +29,14 @@ job_runtime=$SECONDS
 
 container_status "nextcloud"
 if [ $? -eq 0 ]; then
-    # notification "error" "job failed (nextcloud not running)!"
+    notification "error" "job failed (nextcloud not running)!"
+    exit 1
+fi
+
+# run cronjob: nextcloud
+container_exec "nextcloud" "php -f /var/www/html/cron.php" "www-data"
+if [ $? -ne 0 ]; then
+    notification "error" "job failed (error while running command)!"
     exit 1
 fi
 
