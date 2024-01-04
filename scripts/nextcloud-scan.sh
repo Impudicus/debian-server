1f10e72c2263d265ad12a32cd9390c5c171218f0
Impudicus <141483926+Impudicus@users.noreply.github.com>
2024-01-04 23:14:37 +0100

    new commit

---
+++ scripts/nextcloud-scan.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Copyright 2023 by Philipp Hildebrandt
+
+container_exec()
+{
+    /usr/bin/docker exec -u $3 -t $1 $2
+    return $?
+}
+container_status()
+{
+    result=$(/usr/bin/docker inspect -f "{{.State.Status}}" "$1")
+    if [ "$result" = "running" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+notification()
+{
+    /usr/local/bin/notification-push.sh "nextcloud" "$1" "$2"
+    return $?
+}
+
+# ========================= ========================= =========================
+# MAIN
+
+job_runtime=$SECONDS
+
+container_status "nextcloud"
+if [ $? -eq 0 ]; then
+    notification "error" "job failed (nextcloud not running)!"
+    exit 1
+fi
+
+# scan new user-folders
+container_exec "nextcloud" "php occ files:scan --all" "www-data"
+if [ $? -ne 0 ]; then
+    notification "error" "job failed (error while running command)!"
+    exit 1
+fi
+
+# scan new group-folders
+container_exec "nextcloud" "php occ groupfolders:scan 1" "www-data"
+if [ $? -ne 0 ]; then
+    notification "error" "job failed (error while running command)!"
+    exit 1
+fi
+
+container_exec "nextcloud" "php occ groupfolders:scan 2" "www-data"
+if [ $? -ne 0 ]; then
+    notification "error" "job failed (error while running command)!"
+    exit 1
+fi
+
+exit 0
