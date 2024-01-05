0a756e7120555224888bed9db8632a7a213048c5
Impudicus <141483926+Impudicus@users.noreply.github.com>
2024-01-05 21:36:47 +0100

    new commit

--- scripts/restic-init.sh
+++ scripts/restic-init.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # Copyright 2023 by Philipp Hildebrandt
 
+backup_show()
+{
+    backup_dir="/pool1/backup/$HOSTNAME"
+    /usr/bin/restic \
+        -r "sftp:$1:$backup_dir" \
+        snapshots \
+        --password-file "/root/.config/restic/password"
+    return $?
+}
+
+connect_check()
+{
+    result=$(ssh -q -o "BatchMode=yes" root@$1 "echo Fine")
+    return $result
+}
+
 device_slave()
 {
     if [ "$1" = "TS473a" ]; then
@@ -20,11 +36,7 @@ device_status()
     return $result
 }
 
-connect_check()
-{
-    result=$(ssh -q -o "BatchMode=yes" root@$1 "echo Fine")
-    return $result
-}
+
 
 notification()
 {
@@ -52,6 +64,12 @@ fi
 connect_check $slave_name
 if [ $? -ne 0 ]; then
     notification "error" "job failed (unable to connect to slave)!"
+    exit 1
+fi
+
+backup_show $slave_name
+if [ $? -ne 0 ]; then
+    notification "error" "job failed (unable to locate repository on slave)!"
     exit 1
 fi
 
