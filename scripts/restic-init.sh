d5605c45ebdabac292f19b5abd78fae1ad4a7ae0
Impudicus <141483926+Impudicus@users.noreply.github.com>
2024-01-05 21:53:31 +0100

    new commit

---
+++ scripts/restic-init.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Copyright 2023 by Philipp Hildebrandt
+
+repo_check()
+{
+    /usr/bin/restic \
+        -r "sftp:$1:$2" \
+        check \
+        --password-file "/root/.config/restic/password" \
+        1> /dev/null 2> /dev/null
+    return $?
+}
+repo_create()
+{
+
+    return $?
+}
+
+connect_check()
+{
+    /usr/bin/ssh -q \
+        -o "BatchMode=yes" \
+        root@$1 \
+        "echo Fine" \
+        1> /dev/null 2> /dev/null
+    return $?
+}
+
+device_slave()
+{
+    if [ "$1" = "TS473a" ]; then
+        echo "TS673a"
+        return 0
+    fi
+    if [ "$1" = "TS673a" ]; then
+        echo "TS473a"
+        return 0
+    fi
+    return 1
+}
+device_status()
+{
+    timeout=2
+    result=$(/usr/bin/ping -c 1 -W $timeout $1 >/dev/null;)
+    return $result
+}
+
+
+
+notification()
+{
+    /usr/local/bin/notification-push.sh "restic" "$1" "$2"
+    return $?
+}
+
+# ========================= ========================= =========================
+# MAIN
+
+job_runtime=$SECONDS
+
+slave_name="$(device_slave $HOSTNAME)"
+if [ $? -ne 0 ]; then
+    notification "error" "init failed (unable to get device slave)!"
+    exit 1
+fi
+
+device_status $slave_name
+if [ $? -ne 0 ]; then
+    notification "error" "init failed (slave not online)!"
+    exit 1
+fi
+
+connect_check $slave_name
+if [ $? -ne 0 ]; then
+    notification "error" "init failed (unable to connect to slave)!"
+    exit 1
+fi
+
+repository="/pool1/backup/$HOSTNAME"
+
+backup_check $slave_name $repository
+if [ $? -eq 0 ]; then
+    notification "error" "init failed (repository already exists)!"
+    exit 1
+fi
+
+# backup_create $slave_name $repository
+# if [ $? -ne 0 ]; then
+#     notification "error" "backup failed (error while creating backup)!"
+#     exit 1
+# fi
+
+job_duration=$(($SECONDS - runtime))
+notification "okay" "backup finished successfully (runtime: $job_duration sec)!"
+exit 0
