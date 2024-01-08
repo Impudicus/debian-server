fee9fa4343c3068c73f2e1c2f24b7b4a515917ce
Impudicus <141483926+Impudicus@users.noreply.github.com>
2024-01-08 07:47:15 +0100

    new commit

--- scripts/system-poweroff.sh
+++ scripts/system-poweroff.sh
@@ -10,11 +10,10 @@ notification()
 # ========================= ========================= =========================
 # MAIN
 
+notification "info" "system shutting down!"
+
 /usr/sbin/poweroff
 if [ $? -ne 0 ]; then
     notification "error" "backup failed (error while shutting down)!"
     exit 1
-else
-    notification "info" "system shutting down!"
-    exit 0
 fi
