d629cad84e50e6aba2a5eabe959adb8df75fec33
Philipp Hildebrandt <141483926+Impudicus@users.noreply.github.com>
2024-06-03 13:39:26 +0200

    nextcloud added

---
+++ scripts/nextcloud-cron.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# constants
+readonly script_name=${BASH_SOURCE[0]}
+readonly script_path=$(dirname $(realpath ${BASH_SOURCE[0]}))
+readonly script_start=${SECONDS}
+
+# configurations
+# set -o errexit  # exit on error
+# set -o pipefail # return exit status on pipefail
+
+getContainerRunstate() {
+    local container_name="${1}"
+    local container_runstate=$(docker inspect --format "{{.State.Status}}" "${container_name}")
+    if [[ "${container_runstate}" == 'running' ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+printLog() {
+    local log_type="${1}"
+    local log_text="${2}"
+
+    case "${log_type}" in
+        error)
+            printf "${script_name}: \e[41m${log_text}\e[0m\n" >&2
+            ;;
+        okay)
+            printf "${script_name}: \e[42m${log_text}\e[0m\n" >&1
+            ;;
+        info)
+            printf "${script_name}: \e[44m${log_text}\e[0m\n" >&1
+            ;;
+        *)
+            printf "${script_name}: ${log_text}\n" >&1
+            ;;
+    esac
+}
+
+printHelp() {
+    printf "Usage: ${script_name} [OPTIONS]\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message.\n"
+    printf "\n"
+}
+
+main() {
+    # pre-checks
+    if [[ "${EUID}" -ne 0 ]]; then
+        printLog "error" "Script has to be run with root user privileges."
+        exit 1
+    fi
+
+    # variables
+
+    # parameters
+    while [[ $# -gt 0 ]]; do
+        case "${1}" in
+            -h | --help)
+                printHelp
+                exit 0
+                ;;
+            *)
+                printLog "error" "Unknown option '${1}', use --help for further information."
+                exit 1
+                ;;
+        esac
+    done
+
+    # run
+    local check_container='nextcloud'
+    getContainerRunstate "${check_container}"
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+
+    local container_name='nextcloud'
+    docker exec -u www-data -t "${container_name}" php -f /var/www/html/cron.php
+    if [[ $? -ne 0 ]]; then
+        printLog "error" "Job failed! Reason: Error while running command!"
+        exit 1
+    fi
+}
+
+main "$@"
