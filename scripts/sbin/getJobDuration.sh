bb79a078c4917c9f2f696a349e485e70eab6e8ca
Philipp Hildebrandt <141483926+Impudicus@users.noreply.github.com>
2024-06-05 11:14:22 +0200

    new commit

---
+++ scripts/sbin/getJobDuration.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+getJobDuration() {
+    local script_start="${1}"
+    local duration=$((SECONDS - script_start))
+    local hours=$((duration / 3600))
+    local minutes=$(( (duration % 3600) / 60 ))
+    local seconds=$((duration % 60))
+    local result=""
+
+    (( hours > 0 )) && result+="${hours} hours"
+    (( minutes > 0 )) && result+="${result:+, }${minutes} minutes"
+    (( seconds > 0 )) && result+="${result:+, }${seconds} seconds"
+
+    echo "${result}"
+}
