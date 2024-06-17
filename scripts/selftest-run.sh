0566744184ddedf723307aadb447bf47a7cc6e2d
Philipp Hildebrandt <141483926+Impudicus@users.noreply.github.com>
2024-06-17 08:48:20 +0200

    new commit

---
+++ scripts/selftest-run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# constants
+readonly script_name=${BASH_SOURCE[0]}
+readonly script_path=$(dirname $(realpath ${BASH_SOURCE[0]}))
+readonly script_start=${SECONDS}
+
+checkServiceRunstate() {
+    local attempt=1
+    local max_attempts=$(max_attemts)
+    while [ ${attempt} -le ${max_attempts} ]; do
+
+        local service_name="docker"
+        local result=$(systemctl is-active --quiet "${service_name}")
+        if [[ ${result} ]]; then
+            echo "true"
+        else
+            echo "false"
+        fi
+
+        sleep ${max_waittime}
+
+        attempt=$((attempt + 1))
+    done
+}
+
+printLog() {
+    local log_type="${1}"
+    local log_text="${2}"
+
+    case "${log_type}" in
+        error)
+            /usr/local/sbin/pushNotification.sh "debian" "${log_type}" "${log_text}"
+            printf "${script_name}: \e[41m${log_text}\e[0m\n" >&2
+            ;;
+        okay)
+            /usr/local/sbin/pushNotification.sh "debian" "${log_type}" "${log_text}"
+            printf "${script_name}: \e[42m${log_text}\e[0m\n" >&1
+            ;;
+        info)
+            /usr/local/sbin/pushNotification.sh "debian" "${log_type}" "${log_text}"
+            printf "${script_name}: \e[44m${log_text}\e[0m\n" >&1
+            ;;
+        *)
+            printf "${script_name}: ${log_text}\n" >&1
+            ;;
+    esac
+}
+printHelp() {
+    printf "Usage: ${script_name} [OPTIONS]\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message.\n"
+    printf "\n"
+}
+
+main() {
+    # pre-checks
+    if [[ "${EUID}" -ne 0 ]]; then
+        printLog "error" "Script has to be run with root user privileges."
+        exit 1
+    fi
+
+    # variables
+    max_attemts=3
+    max_waittime=30
+
+    # parameters
+    while [[ $# -gt 0 ]]; do
+        case "${1}" in
+            -h | --help)
+                printHelp
+                exit 0
+                ;;
+            *)
+                printLog "error" "Unknown option '${1}', use --help for further information."
+                exit 1
+                ;;
+        esac
+    done
+
+    # run
+    checkServiceRunstate
+
+
+    exit 0
+}
+
+main "$@"
